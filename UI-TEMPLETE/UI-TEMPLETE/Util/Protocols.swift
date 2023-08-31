bfb6d16caafb173d412223e6f8cecf653caec280
Khan1201 <caa789@naver.com>
2023-08-31 14:46:44 +0900

    Add: selectable 프로토콜 추가

---
+++ UI-TEMPLETE/UI-TEMPLETE/Util/Protocols.swift
@@ -0,0 +1,13 @@
+//
+//  Protocols.swift
+//  UI-TEMPLETE
+//
+//  Created by 윤형석 on 2023/08/31.
+//
+
+import Foundation
+
+protocol Selectable {
+  var title: String { get }
+  var value: String { get }
+}
