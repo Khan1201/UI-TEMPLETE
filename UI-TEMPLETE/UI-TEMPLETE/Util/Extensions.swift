5a29c5f11d5744b4e28504390c9f00db2b02aa24
Khan1201 <caa789@naver.com>
2023-08-31 15:08:20 +0900

    Add UIApplication extension, 각 extension별 설명 추가

--- UI-TEMPLETE/UI-TEMPLETE/Util/Extensions.swift
+++ UI-TEMPLETE/UI-TEMPLETE/Util/Extensions.swift
@@ -8,6 +8,29 @@
 import Foundation
 import SwiftUI
 
+// MARK: - String
+
+extension String {
+  
+  var toInt: Int {
+    return Int(self) ?? 0
+  }
+}
+// MARK: - Int
+
+extension Int {
+  
+  func toTwoDigitIfOneDigit() -> Int {
+    var toString = String(self)
+    guard toString.count == 1 else { return self}
+    toString.insert("0", at: toString.startIndex)
+    
+    return Int(toString) ?? self
+  }
+}
+
+// MARK: - Date
+
 extension Date {
   
   func toStringByFormat(_ format: String) -> String {
@@ -25,29 +48,15 @@ extension Date {
   }
 }
 
-extension String {
-  
-  var toInt: Int {
-    return Int(self) ?? 0
-  }
-}
-
-extension Int {
-  
-  func toTwoDigitIfOneDigit() -> Int {
-    var toString = String(self)
-    guard toString.count == 1 else { return self}
-    toString.insert("0", at: toString.startIndex)
-    
-    return Int(toString) ?? self
-  }
-}
+// MARK: - UIScreen
 
 extension UIScreen{
    static let screenWidth = UIScreen.main.bounds.size.width
    static let screenHeight = UIScreen.main.bounds.size.height
    static let screenSize = UIScreen.main.bounds.size
 }
+
+// MARK: - View
 
 extension View {
   func snapShot() -> UIImage {
@@ -66,8 +75,41 @@ extension View {
   }
 }
 
+// MARK: - Color
+
 extension Color {
   static func rgb(_ r: CGFloat, _ g: CGFloat, _ b: CGFloat) -> Color {
     return Color(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0))
   }
+}
+
+// MARK: - UIApplication
+
+extension UIApplication {
+  
+  func enableHidingKeyboardOnTapOutside() {
+    
+    let scenes = UIApplication.shared.connectedScenes
+    let windowScene = scenes.first as? UIWindowScene
+    
+    guard let window = windowScene?.windows.first else { return }
+    
+    let tapRecognizer = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+    tapRecognizer.cancelsTouchesInView = false
+    tapRecognizer.delegate = self
+    window.addGestureRecognizer(tapRecognizer)
+  }
+  
+  func hideKeyboard() {
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+  }
+}
+
+extension UIApplication: UIGestureRecognizerDelegate {
+  public func gestureRecognizer(
+    _ gestureRecognizer: UIGestureRecognizer,
+    shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer)
+  -> Bool {
+      return false
+    }
 }
