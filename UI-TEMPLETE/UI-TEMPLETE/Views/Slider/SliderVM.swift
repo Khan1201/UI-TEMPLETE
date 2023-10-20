8acef62dc4498a21258d93450b409e48275f9468
Khan1201 <caa789@naver.com>
2023-10-20 09:18:31 +0900

    Update 슬라이더 View, View model

--- UI-TEMPLETE/UI-TEMPLETE/Views/Slider/SliderVM.swift
+++ UI-TEMPLETE/UI-TEMPLETE/Views/Slider/SliderVM.swift
@@ -14,13 +14,29 @@ final class SliderVM: ObservableObject {
   @Published var stepedCount: CGFloat = 0
   @Published var touchEnded: Bool = false
   
-  let minMaxCount: (CGFloat, CGFloat) = (1, 5)
+  let height: CGFloat = 5
+  let minCount: Int = 1
+  let maxCount: Int = 5
+  let circleWidthHeight: CGFloat = 16
+  var stepWidth: CGFloat = 0
+  
+  var isDragAvailable: Bool {
+    return Int(stepedCount) < Int(maxCount) - 1
+  }
+  
+  var fillCircleLeadingPadding: CGFloat {
+    return filledWidth == 0 ? -(circleWidthHeight / 2) : filledWidth - circleWidthHeight
+  }
+  
+  var scoreYOffset: CGFloat {
+    return circleWidthHeight + 5 // 5 = vertical padding
+  }
 }
 
 // MARK: - Gestures..
 
 extension SliderVM {
-  func resetButtonOnTapGesture(stepWidth: CGFloat) {
+  func resetButtonOnTapGesture() {
     filledWidth = 0
     nextStepWidth = stepWidth
     stepedCount = 1
@@ -31,21 +47,20 @@ extension SliderVM {
 // MARK: - Detect actions..
 
 extension SliderVM {
-  func dragGestureOnChangedAction(transWidth: CGFloat, stepWidth: CGFloat) {
+  func dragGestureOnChangedAction(transWidth: CGFloat) {
     
     // 오른쪽으로 쭉 이동
     // 만약 한번 때고 다시 이동 시, endWidth 더함
     if transWidth + (touchEnded ? endedWidth : 0) >=
-        nextStepWidth - (stepedCount * 15) && stepedCount < minMaxCount.1 {
-      
-      filledWidth += stepWidth
-      nextStepWidth += stepWidth
-      stepedCount += 1
+        nextStepWidth - (stepedCount * circleWidthHeight) && stepedCount < CGFloat(maxCount) - 1 {
+        filledWidth += stepWidth
+        nextStepWidth += stepWidth
+        stepedCount += 1
       
     // 왼쪽으로 쭉 이동
     // 만약 한번 때고 다시 이동 시, endWidth 더함
     } else if transWidth + (touchEnded ? endedWidth : 0) <=
-                filledWidth - stepWidth + 15 && stepedCount >= minMaxCount.0 {
+                filledWidth - stepWidth + circleWidthHeight && stepedCount >= CGFloat(minCount) {
       filledWidth -= stepWidth
       nextStepWidth -= stepWidth
       stepedCount -= 1
@@ -61,7 +76,44 @@ extension SliderVM {
 // MARK: - Appear(onAppear, task)
 
 extension SliderVM {
-  func viewWillAppearAction(stepWidth: CGFloat) {
+  func viewOnAppearAction(
+    screenWidth: CGFloat,
+    horizontalPadding: CGFloat
+  ) {
+    setStepWidth(screenWidth: screenWidth, horizontalPadding: horizontalPadding)
     nextStepWidth = stepWidth
+  }
+}
+
+// MARK: - Return funcs..
+
+extension SliderVM {
+  func emptyCircleLeadingPadding(_ index: Int) -> CGFloat {
+    if index == 0 {
+      return -(circleWidthHeight / 2)
+      
+    } else {
+      return stepWidth * CGFloat(index) - circleWidthHeight
+    }
+  }
+  
+  func scoreXOffset(_ index: Int) -> CGFloat {
+    if index == 1 {
+      return -10
+      
+    } else {
+      return stepWidth * CGFloat(index - 1) - 16
+    }
+  }
+}
+
+// MARK: - ETC funcs..
+
+extension SliderVM {
+  func setStepWidth(
+    screenWidth: CGFloat,
+    horizontalPadding: CGFloat
+  ) {
+      stepWidth = (screenWidth - horizontalPadding) / (CGFloat(maxCount) - 1)
   }
 }
--- UI-TEMPLETE/UI-TEMPLETE/Views/Slider/SliderView.swift
+++ UI-TEMPLETE/UI-TEMPLETE/Views/Slider/SliderView.swift
@@ -8,62 +8,90 @@
 import SwiftUI
 
 struct SliderView: View {
-  var horizontalPadding: CGFloat = 48
+  
   @StateObject var viewModel: SliderVM = SliderVM()
+  var horizontalPadding: CGFloat = 48
   
   var body: some View {
-    let stepWidth: CGFloat = (UIScreen.screenWidth - horizontalPadding) / 4
     
     VStack(alignment: .leading, spacing: 0) {
-
-      Text("stepWidth: \(stepWidth)")
+      
+      Text("stepWidth: \(viewModel.stepWidth)")
       Text("nextStepWidth: \(viewModel.nextStepWidth)")
       Text("filledWidth: \(viewModel.filledWidth)")
       Text("stepedCount: \(viewModel.stepedCount)")
-
+      
       ZStack(alignment: .leading) {
         RoundedRectangle(cornerRadius: 12)
-          .fill(Color.gray)
-          .frame(maxWidth: .infinity, maxHeight: 5)
+          .fill(Color.init(hexCode: "#EAEAEA"))
+          .frame(
+            maxWidth: .infinity,
+            maxHeight: viewModel.height
+          )
         
         RoundedRectangle(cornerRadius: 12)
-          .fill(Color.blue)
-          .frame(width: viewModel.filledWidth, height: 5)
+          .fill(Color.init(hexCode: "#FF509B"))
+          .frame(
+            maxWidth: viewModel.filledWidth,
+            maxHeight: viewModel.height
+          )
+        
+        ForEach(0..<viewModel.maxCount, id: \.self) { i in
+          Circle()
+            .fill(Int(viewModel.stepedCount) > i ?
+                  Color.init(hexCode: "#FF509B") : Color.init(hexCode: "#EAEAEA")
+            )
+            .frame(
+              maxWidth: viewModel.circleWidthHeight,
+              maxHeight: viewModel.circleWidthHeight
+            )
+            .padding(.leading, viewModel.emptyCircleLeadingPadding(i))
+        }
         
         Circle()
-          .fill(Color.red)
-          .frame(maxWidth: 15, maxHeight: 15)
-          .padding(.leading, viewModel.filledWidth == 0 ?
-                   viewModel.filledWidth : viewModel.filledWidth - 15)
+          .fill(Color.init(hexCode: "#FF509B"))
+          .frame(
+            maxWidth: viewModel.circleWidthHeight,
+            maxHeight: viewModel.circleWidthHeight
+          )
+          .padding(.leading, viewModel.fillCircleLeadingPadding)
           .gesture(
             DragGesture()
               .onChanged { value in
                 viewModel.dragGestureOnChangedAction(
-                  transWidth: value.translation.width,
-                  stepWidth: stepWidth
+                  transWidth: value.translation.width
                 )
               }
               .onEnded { value in
                 viewModel.dragGestureOnEndedAction()
               }
           )
+        
+        ForEach(viewModel.minCount...viewModel.maxCount, id: \.self) { i in
+          Text("\(i)점")
+            .font(.system(size: 14, weight: .medium))
+            .offset(x: viewModel.scoreXOffset(i), y: viewModel.scoreYOffset)
+        }
       }
       .padding(.top, 20)
       
       Text("Reset")
         .padding()
-        .overlay(content: {
+        .overlay {
           RoundedRectangle(cornerRadius: 12)
             .strokeBorder(Color.gray)
-        })
-        .onTapGesture {
-          viewModel.resetButtonOnTapGesture(stepWidth: stepWidth)
         }
-        .padding(.top, 20)
+        .onTapGesture {
+          viewModel.resetButtonOnTapGesture()
+        }
+        .padding(.top, 40)
     }
     .padding(.horizontal, 24)
-    .task {
-      viewModel.viewWillAppearAction(stepWidth: stepWidth)
+    .onAppear {
+      viewModel.viewOnAppearAction(
+        screenWidth: UIScreen.screenWidth,
+        horizontalPadding: horizontalPadding
+      )
     }
   }
 }
